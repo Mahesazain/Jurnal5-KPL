b691d02231c36e42c27d62f1924110273e125764
mahesazain <echa.zain@gmail.com>
2024-03-20 07:51:12 +0700

    menambahkan class simpleDatabse

--- Program.cs
+++ Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MOD5_1302220105;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOD5_1302220105
+{
+     class simpleDatabase<T>
+    {
+        private List<T> storedData;
+        private List<DateTime> inputDates;
+
+        public simpleDatabase()
+        {
+            this.storedData = new List<T>();
+            this.inputDates = new List<DateTime>();
+        }
+        public void AddNewData(T input)
+        {
+            storedData.Add(input);
+            inputDates.Add(DateTime.Now);
+        }
+
+        public void PrintAllData()
+        {
+            for (int i = 0; i < this.storedData.Count(); i++)
+            {
+                Console.WriteLine("Data " + (i + 1) + " Berisi data: " + this.storedData[i] + ", yang disimpan pada waktu UTC: " + this.inputDates[i]+" AM");
+            }
+        }
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        simpleDatabase<double> T1 = new simpleDatabase<double>();
+        T1.AddNewData(1.3);
+        T1.AddNewData(0.2);
+        T1.AddNewData(2.0);
+        T1.PrintAllData();
+    }
+}
+
--- obj/Debug/net8.0/MOD5_1302220105.AssemblyInfo.cs
+++ obj/Debug/net8.0/MOD5_1302220105.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MOD5_1302220105")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eabeb5af597e35187fb2b3298836b14c43b66849")]
 [assembly: System.Reflection.AssemblyProductAttribute("MOD5_1302220105")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MOD5_1302220105")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
